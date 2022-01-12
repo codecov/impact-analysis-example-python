54123036d282100e81acb7939604863764cf952b
Tom Hu <tomhu1096+gpg@gmail.com>
2022-01-12 13:42:39 -0500

    Show endpoint change

--- example-app/utils/tests/test_time.py
+++ example-app/utils/tests/test_time.py
@@ -4,4 +4,4 @@ from ..time import format_time
 
 def test_time():
     current_time = datetime.now()
-    assert(format_time(current_time) == datetime.strftime(current_time, '%Y-%m-%d %H:%M:%S'))
+    assert(format_time(current_time) == datetime.strftime(current_time, '%Y-%m-%d%H:%M:%S'))
