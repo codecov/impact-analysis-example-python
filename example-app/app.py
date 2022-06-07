5ad599fdd339043a8815e39155ab1bfd12511505
Tom Hu <tom@codecov.io>
2022-06-07 17:11:16 -0400

    test: hit the endpoint 100 times

--- example-app/app.py
+++ example-app/app.py
@@ -60,7 +60,7 @@ FlaskInstrumentor().instrument_app(app)
 def index():
     return render_template('main.html')
 
-@app.route('/time1')
+@app.route('/time')
 def current_time():
     return render_template(
         'time.html',
